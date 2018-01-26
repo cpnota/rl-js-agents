749f400411eafc94f442a240360e4061616c94c3
Chris Nota <cpnota@gmail.com>
2018-01-25 21:04:08 -0500

    initial sarsa impl

---
+++ sarsa/index.js
@@ -0,0 +1,33 @@
+module.exports = class Sarsa {
+  constructor({ q, policy }) {
+    this.q = q
+    this.policy = policy
+  }
+
+  newEpisode(environment) {
+    this.environment = environment
+    this.state = this.environment.getState()
+    this.action = this.policy.chooseAction(this.state)
+  }
+
+  act() {
+    this.environment.dispatch(this.action)
+    this.nextState = this.environment.getState()
+    this.nextAction = this.policy.chooseAction(this.nextState)
+    this.updateWeights()
+    this.state = this.nextState
+    this.action = this.nextAction
+  }
+
+  updateWeights() {
+    const estimate = this.q.call(this.state, this.action)
+    const nextEstimate = this.environment.isTerminated()
+      ? 0
+      : this.q.score(this.nextState, this.nextAction)
+    const error =
+      this.environment.getReward() +
+      this.environment.gamma * nextEstimate -
+      estimate
+    this.q.update(this.state, this.action, error)
+  }
+}
