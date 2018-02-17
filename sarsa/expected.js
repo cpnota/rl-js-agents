1fcabe1b95a5da165b47d150295bdc0a2197f3ac
Chris Nota <cpnota@gmail.com>
2018-02-17 12:33:08 -0500

    add expected-sarsa

---
+++ sarsa/expected.js
@@ -0,0 +1,48 @@
+const math = require('mathjs')
+
+module.exports = class ExpectedSarsa {
+  constructor({ q, policy }) {
+    this.q = q
+    this.policy = policy
+  }
+
+  newEpisode(environment) {
+    this.environment = environment
+    this.state = this.environment.getState()
+    this.action = this.policy.chooseAction(this.state)
+  }
+
+  act() {
+    this.environment.dispatch(this.action)
+    this.nextState = this.environment.getState()
+    this.nextAction = this.policy.chooseAction(this.nextState)
+    this.q.update(this.state, this.action, this.getTDError())
+    this.state = this.nextState
+    this.action = this.nextAction
+  }
+
+  getTDError() {
+    const nextEstimate = this.environment.isTerminated()
+      ? 0
+      : this.getExpected()
+
+    const estimate = this.q.call(this.state, this.action)
+
+    return (
+      this.environment.getReward() +
+      this.environment.gamma * nextEstimate -
+      estimate
+    )
+  }
+
+  getExpected() {
+    const actions = Object.values(this.environment.actions)
+    return math.sum(
+      ...actions.map(action => {
+        const value = this.q.call(this.nextState, action)
+        const probability = this.q.probability(this.nextState, action)
+        return value * probability
+      })
+    )
+  }
+}
