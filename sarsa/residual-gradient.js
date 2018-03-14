e94d30926c0251dd99bd2d86172e1531314c0350
Chris Nota <cpnota@gmail.com>
2018-03-14 12:40:54 -0400

    Residual Gradient Sarsa (#10)
    * residual gradiant
    
    * use the more valid residual gradient implementation
    
    * add gamma to res grad

---
+++ sarsa/residual-gradient.js
@@ -0,0 +1,55 @@
+const math = require('mathjs')
+
+module.exports = class Sarsa {
+  constructor({ q, policy, gamma = 1 }) {
+    this.q = q
+    this.policy = policy
+    this.gamma = gamma
+  }
+
+  newEpisode(environment) {
+    this.environment = environment
+    this.state = this.environment.getState()
+    this.action = this.policy.chooseAction(this.state)
+  }
+
+  act() {
+    this.environment.dispatch(this.action)
+    this.nextState = this.environment.getState()
+    this.nextAction = this.policy.chooseAction(this.nextState)
+    this.update()
+    this.state = this.nextState
+    this.action = this.nextAction
+  }
+
+  update() {
+    const tdError = this.getTDError()
+    const errors = math.multiply(
+      tdError,
+      math.subtract(
+        this.q.gradient(this.state, this.action),
+        math.multiply(
+          this.getGamma(),
+          this.q.gradient(this.nextState, this.nextAction)
+        )
+      )
+    )
+    this.q.updateWeights(errors)
+  }
+
+  getTDError() {
+    const nextEstimate = this.environment.isTerminated()
+      ? 0
+      : this.q.call(this.nextState, this.nextAction)
+
+    const estimate = this.q.call(this.state, this.action)
+
+    return (
+      this.environment.getReward() + this.getGamma() * nextEstimate - estimate
+    )
+  }
+
+  getGamma() {
+    return this.environment.gamma * this.gamma
+  }
+}
